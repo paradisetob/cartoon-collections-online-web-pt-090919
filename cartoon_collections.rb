8f3cd4efd80efca0fae88396d28d109e7251b243
tracey brimage <onelovenilynn@gmail.com>
2019-09-30 03:56:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ end
 
 def summon_captain_planet (summon_captain_planet)
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-summon_captain_planet(planeteer_calls)
+planeteer_calls.map.uppercase
 end
 
 def long_planeteer_calls# code an argument here
