eac7a38c3d0fcc65c5f2c9126fefdc873946f87e
tracey brimage <onelovenilynn@gmail.com>
2019-09-30 04:13:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,11 +4,12 @@ end
 
 def summon_captain_planet (summon_captain_planet)
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-planeteer_calls.each 
+planeteer_calls.each
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(assorted_words)
+  assorted_words = ["two", "go", "industrious", "bop"]
+  long_planeteer_calls(assorted_words)
 end
 
 def find_the_cheese# code an argument here
