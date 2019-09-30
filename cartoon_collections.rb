ecd3e6b3d0e4c94e7cc11626b30c03f18ef9a17c
tracey brimage <onelovenilynn@gmail.com>
2019-09-30 03:55:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,9 @@ def roll_call_dwarves (cartoon-collections)
 puts ["Doc", "Dopey", "Bashful", "Grumpy"]
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (summon_captain_planet)
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-
+summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls# code an argument here
